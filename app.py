1e27f20a7fc6d9179701c2b5177b032aa218839f
leticiap-almeida <144292426+leticiap-almeida@users.noreply.github.com>
2024-11-25 19:25:02 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Conjunto de dados de filmes
+filmes = [
+    {"nome": "Mad Max", "genero": "Ação"},
+    {"nome": "Veloses e furiosos", "genero": "Ação"},
+    {"nome": "Toy Story", "genero": "Comédia"},
+    {"nome": "Gente Grande", "genero": "Comédia"},
+    {"nome": "Invocação do Mal", "genero": "Terror"},
+    {"nome": "Annabelle", "genero": "Terror"},
+    {"nome": "O Rei Leão", "genero": "Animação"},
+    {"nome": "Smurf", "genero": "Animação"},
+    {"nome": "Continência ao Amor", "genero": "Romance"},
+    {"nome": "Uma ideia de você", "genero": "Romance"},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recomendar', methods=['POST'])
+def recomendar():
+    # Obtém o gênero preferido do formulário
+    genero_preferido = request.form['genero'].capitalize()
+
+    # Filtra os filmes com o gênero escolhido
+    sugestoes = [f['nome'] for f in filmes if f['genero'] == genero_preferido]
+
+    # Retorna a sugestão em formato JSON
+    if sugestoes:
+        return jsonify(sugestoes=sugestoes)
+    else:
+        return jsonify(sugestoes=[])
+
+if __name__ == '__main__':
+    app.run(debug=True)
